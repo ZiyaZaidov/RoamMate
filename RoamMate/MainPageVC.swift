934a12c7fee36ca7383690347537c8c9c7844104
Ziya <iosdeveloper82@gmail.com>
2023-07-31 17:52:22 +0400

    test

--- RoamMate/MainPageVC.swift
+++ RoamMate/MainPageVC.swift
@@ -19,15 +19,6 @@ class MainPageVC: UIViewController {
 
 }
 
-extension MainPageVC: UICollectionViewDelegate,UICollectionViewDataSource {
+
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
-    }
-    
-    
-}
+
